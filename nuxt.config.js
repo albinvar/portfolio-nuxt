8ca6e78079536336a38f7e6456f5749fb6e7f40a
Albin Varghese <albin8285@gmail.com>
2022-01-22 14:54:44 +0530

    wip:create Hero section of Index page

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,12 @@ export default {
   plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+  components: {
+    dirs: [
+      '~/components',
+      '~/components/home'
+    ]
+  },
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
-  <Tutorial />
+  <HeroComponent />
 </template>
 
 <script>
 export default {
-  name: 'IndexPage',
+  name: 'IndexPage'
 }
 </script>
