bb872c473e784135b36c713018ffc9a476ffb456
Albin Varghese <albin8285@gmail.com>
2022-01-22 16:37:25 +0530

    add background pattern

--- pages/index.vue
+++ pages/index.vue
@@ -12,3 +12,19 @@ export default {
   },
 }
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+</style>
