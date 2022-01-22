1e53941c2e6956d6850a6ed027315297e7721bbc
Albin Varghese <albin8285@gmail.com>
2022-01-22 16:45:21 +0530

    install animate.css

--- pages/index.vue
+++ pages/index.vue
@@ -21,10 +21,4 @@ export default {
   text-align: center;
   color: #2c3e50;
 }
-.fade-enter-active, .fade-leave-active {
-  transition: opacity .5s;
-}
-.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
-  opacity: 0;
-}
 </style>
