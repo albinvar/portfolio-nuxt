f0ef1f182cceb5c264fbd3d0f584f40ea1cddba9
Albin Varghese <albin8285@gmail.com>
2022-01-23 19:38:04 +0530

    create error pages

---
+++ components/error/NotFoundComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <main class="mx-auto max-w-7xl px-4 my-40 sm:px-6 md:my-52 lg:my-72 lg:px-8 xl:mt-28">
+    <div class="text-center">
+      <h1 class="text-7xl font-extrabold tracking-wide">404</h1>
+      <h3 class="my-8 text-xl font-semibold">Page not found</h3>
+
+      <h5 class="mt-40 text-base font-normal">Return to <NuxtLink to='/' class='text-blue-600'> home page</NuxtLink></h5>
+    </div>
+
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'NotFoundComponent',
+}
+</script>
---
+++ layouts/error.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div v-if="error.statusCode === 404">
+      <NotFoundComponent />
+    </div>
+    <div v-else>
+      <UnknownErrorComponent :error='error' />
+    </div>
+  </div>
+</template>
+
+<script>
+import NotFoundComponent from '~/components/error/NotFoundComponent'
+import UnknownErrorComponent from '~/components/error/UnknownErrorComponent'
+export default {
+  name: 'ErrorLayout',
+  components: { UnknownErrorComponent, NotFoundComponent },
+  props: ['error'],
+  layout: 'error' // you can set a custom layout for the error page
+}
+</script>
