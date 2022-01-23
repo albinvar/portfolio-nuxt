cc5ee8a582990ae51ea2acc89690dc095a179e16
Albin Varghese <albin8285@gmail.com>
2022-01-23 20:20:30 +0530

    show svgs based on the errors

--- components/error/NotFoundComponent.vue
+++ components/error/NotFoundComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="mx-auto max-w-7xl px-4 my-40 sm:px-6 md:my-52 lg:my-72 lg:px-8 xl:mt-28">
     <div class="text-center">
-      <h1 class="text-7xl font-extrabold tracking-wide">404</h1>
+      <object :data='NotFoundSvg' type='image/svg+xml' class='my-3 mx-auto w-1/2 md:w-1/3'></object>
       <h3 class="my-8 text-xl font-semibold">Page not found</h3>
 
       <h5 class="mt-40 text-base font-normal">Return to <NuxtLink to='/' class='text-blue-600'> home page</NuxtLink></h5>
@@ -13,5 +13,10 @@
 <script>
 export default {
   name: 'NotFoundComponent',
+  data() {
+    return {
+      NotFoundSvg: require('~/assets/illustrations/404.svg')
+    }
+  }
 }
 </script>
--- components/error/UnknownErrorComponent.vue
+++ components/error/UnknownErrorComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="mx-auto max-w-7xl px-4 my-40 sm:px-6 md:my-52 lg:my-72 lg:px-8 xl:mt-28">
     <div class="text-center">
-      <h1 class="text-7xl font-extrabold tracking-wide">{{ error.statusCode }}</h1>
+      <object :data='ErrorSvg' type='image/svg+xml' class='my-3 mx-auto w-1/2 md:w-1/3'></object>
       <h3 class="my-8 text-xl font-semibold">An error occurred</h3>
 
       <h5 class="mt-40 text-base font-normal">Return to <NuxtLink to='/' class='text-blue-600'> home page</NuxtLink></h5>
@@ -14,6 +14,11 @@
 export default {
   name: 'UnknownErrorComponent',
   props: ['error'],
+  data() {
+    return {
+      ErrorSvg: require('~/assets/illustrations/error.svg')
+    }
+  }
 }
 </script>
 
